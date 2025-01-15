32b722e4b382f22eb24c347627eb30db639b7822
fro0m <fro0m@users.noreply.github.com>
2025-01-15 13:09:49 +0000

    ✅ Ready to clone and code.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,6 +1,6 @@
 # How to develop on this project
 
-project_name welcomes contributions from the community.
+financial_investments_model welcomes contributions from the community.
 
 **You need PYTHON3!**
 
@@ -8,9 +8,9 @@ This instructions are for linux base systems. (Linux, MacOS, BSD, etc.)
 ## Setting up your own fork of this repo.
 
 - On github interface click on `Fork` button.
-- Clone your fork of this repo. `git clone git@github.com:YOUR_GIT_USERNAME/project_urlname.git`
-- Enter the directory `cd project_urlname`
-- Add upstream repo `git remote add upstream https://github.com/author_name/project_urlname`
+- Clone your fork of this repo. `git clone git@github.com:YOUR_GIT_USERNAME/financial-investments-model.git`
+- Enter the directory `cd financial-investments-model`
+- Add upstream repo `git remote add upstream https://github.com/fro0m/financial-investments-model`
 
 ## Setting up your own virtual environment
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for financial_investments_model package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("financial_investments_model", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="financial_investments_model",
+    version=read("financial_investments_model", "VERSION"),
+    description="Awesome financial_investments_model created by fro0m",
+    url="https://github.com/fro0m/financial-investments-model/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="fro0m",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["financial_investments_model = financial_investments_model.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
