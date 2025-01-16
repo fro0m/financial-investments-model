e6b324e34581735b19428ec902bf4667259a9ab8
Your Name <youremail@yourdomain.com>
2025-01-17 00:30:54 +0300

    fixed mistakes and removed unused data

--- financial_plan/cash_flow_calc.py
+++ financial_plan/cash_flow_calc.py
@@ -11,8 +11,7 @@ number_of_months : int = 100*12 # calculate for 100 years forward
 average_days_in_month : float = 30.436875
 cumulative_inflation_rate: np.array = np.cumprod(np.full(number_of_months, 1 + 0.09/12)) # let's say that yearly inflation is 9%
 cumulative_realty_price_change_monthly_rate: np.array = np.cumprod(np.full(number_of_months, 1 + 0.10/12)) # https://www.perplexity.ai/search/srednii-rost-tseny-zhiloi-nedv-5xtf_BFOTbeLgEYBmmSPKg
-cumulative_realty_rent_change_monthly_rate: np.array = np.full(number_of_months, 1 + 0.04/12) # https://www.perplexity.ai/search/srednii-rost-tseny-arendy-zhil-lhvgH9HnTGmuhlsJHdI6xg
-cumulative_cash_conservative_investment_rate: np.array = np.full(number_of_months, 1 + 0.13/12) # basic rate of investment cash to a bank deposit or goverment bonds. This is used to adjust costs as they would be invested in the conservative tools
+cumulative_realty_rent_change_monthly_rate: np.array = np.cumprod(np.full(number_of_months, 1 + 0.04/12)) # https://www.perplexity.ai/search/srednii-rost-tseny-arendy-zhil-lhvgH9HnTGmuhlsJHdI6xg
 
 @dataclass
 class AssetBase:
@@ -137,19 +136,18 @@ class RealtyObject(AssetBase):
   cumulative_inflation_rate: np.array
   cumulative_realty_price_change_monthly_rate: np.array
   cumulative_realty_rent_change_monthly_rate: np.array
-  cumulative_cash_conservative_investment_rate: np.array
 
   def date_ready_to_get_income(self) -> date:
     return self.date_of_getting_keys + relativedelta(months=self.renovation_time_months)
 
   def instant_costs(self, by_month_end_index=1) -> int:
-    month = by_month_end_index - 1
+    
     months_to_renovation_start = floor((self.date_of_getting_keys - date.today()).days / average_days_in_month)
     if months_to_renovation_start < 0: # means that object already ready to use as current date is after date of building the realty
       instant_costs = self.instant_price_rur + self.instant_price_renovation_rur
-    elif month > months_to_renovation_start: # means that renovation has started       
+    elif by_month_end_index > months_to_renovation_start: # means that renovation has started       
       instant_costs = self.instant_price_rur + self.instant_price_renovation_rur
-    elif month <= months_to_renovation_start: # means that renovation has not started
+    elif by_month_end_index <= months_to_renovation_start: # means that renovation has not started
       instant_costs = self.instant_price_rur 
     else:
       raise Exception("Logic error occurred")  # logic error
@@ -157,37 +155,34 @@ class RealtyObject(AssetBase):
     return instant_costs
   
   def monthly_costs(self, by_month_end_index=1) -> int :
-    month = by_month_end_index - 1
-    if month > self.realty_mortgage_principal_and_interest_payments_months:
+    if by_month_end_index > self.realty_mortgage_principal_and_interest_payments_months:
       realty_mortgage_principal_and_interest_rur = 0 # loan already covered
     else:
       realty_mortgage_principal_and_interest_rur = self.realty_mortgage_principal_and_interest_rur
 
-    if month > self.renovation_principal_and_interest_payments_months:
+    if by_month_end_index > self.renovation_principal_and_interest_payments_months:
       renovation_principal_and_interest_rur = 0 # loan already covered
     else:
       renovation_principal_and_interest_rur = self.renovation_principal_and_interest_rur
     
-    result_in_current_month = (self.income_tax_percentage * self.monthly_income(month) + renovation_principal_and_interest_rur + \
+    result_in_current_month = (self.income_tax_percentage * self.monthly_income(by_month_end_index) + renovation_principal_and_interest_rur + \
     realty_mortgage_principal_and_interest_rur + (self.cap_ex_rur + \
     self.property_management_rur + self.insurance_rur + \
-    self.additional_monthly_expenses_rur + self.utilities_rur) * cumulative_inflation_rate[month])
+    self.additional_monthly_expenses_rur + self.utilities_rur) * cumulative_inflation_rate[by_month_end_index])
 
     return result_in_current_month
 
   def instant_income(self, by_month_end_index=1) -> int :
-    month = by_month_end_index - 1
-    return self.realty_object_price_rub * self.cumulative_realty_price_change_monthly_rate[month]
+    return self.realty_object_price_rub * self.cumulative_realty_price_change_monthly_rate[by_month_end_index]
 
-  def monthly_income(self, by_month_end_index=1) -> int :
-    month = by_month_end_index - 1
+  def monthly_income(self, by_month_end_index=1) -> int : 
     return (self.expected_monthly_rent_rur + self.additional_income_rur) * self.vacancy_percentage * \
-      self.cumulative_realty_rent_change_monthly_rate[month]
+      self.cumulative_realty_rent_change_monthly_rate[by_month_end_index]
 
 
-avant_appartment_fifth_percent_mortgage = RealtyObject(name = 'Avant 5 percent mortgage',  instant_price_rur=4000000, instant_price_renovation_rur=2500000, renovation_principal_and_interest_rur=0, renovation_principal_and_interest_payments_months=0, realty_mortgage_principal_and_interest_rur=71796, realty_mortgage_principal_and_interest_payments_months=30*12, cap_ex_rur=5000, income_tax_percentage=0.07, property_management_rur=20000, insurance_rur=0, additional_monthly_expenses_rur=0, utilities_rur=0, date_of_getting_keys=date(2027, 6, 1),  renovation_time_months=3, realty_object_price_rub=22000000, expected_monthly_rent_rur=120000, additional_income_rur=0,vacancy_percentage=0.9, cumulative_inflation_rate=cumulative_inflation_rate, cumulative_realty_price_change_monthly_rate=cumulative_realty_price_change_monthly_rate, cumulative_realty_rent_change_monthly_rate=cumulative_realty_rent_change_monthly_rate,cumulative_cash_conservative_investment_rate=cumulative_cash_conservative_investment_rate)
+avant_appartment_fifth_percent_mortgage = RealtyObject(name = 'Avant 5 percent mortgage',  instant_price_rur=4000000, instant_price_renovation_rur=2500000, renovation_principal_and_interest_rur=0, renovation_principal_and_interest_payments_months=0, realty_mortgage_principal_and_interest_rur=71796, realty_mortgage_principal_and_interest_payments_months=30*12, cap_ex_rur=5000, income_tax_percentage=0.07, property_management_rur=20000, insurance_rur=0, additional_monthly_expenses_rur=0, utilities_rur=0, date_of_getting_keys=date(2027, 6, 1),  renovation_time_months=3, realty_object_price_rub=22000000, expected_monthly_rent_rur=120000, additional_income_rur=0,vacancy_percentage=0.9, cumulative_inflation_rate=cumulative_inflation_rate, cumulative_realty_price_change_monthly_rate=cumulative_realty_price_change_monthly_rate, cumulative_realty_rent_change_monthly_rate=cumulative_realty_rent_change_monthly_rate)
 
-avant_appartment = RealtyObject(name = 'Avant',  instant_price_rur=6000000, instant_price_renovation_rur=2500000, renovation_principal_and_interest_rur=0, renovation_principal_and_interest_payments_months=0, realty_mortgage_principal_and_interest_rur=254222, realty_mortgage_principal_and_interest_payments_months=30*12, cap_ex_rur=5000, income_tax_percentage=0.07, property_management_rur=20000, insurance_rur=0, additional_monthly_expenses_rur=0, utilities_rur=0, date_of_getting_keys=date(2027, 6, 1),  renovation_time_months=3, realty_object_price_rub=22000000, expected_monthly_rent_rur=120000, additional_income_rur=0,vacancy_percentage=0.9, cumulative_inflation_rate=cumulative_inflation_rate, cumulative_realty_price_change_monthly_rate=cumulative_realty_price_change_monthly_rate, cumulative_realty_rent_change_monthly_rate=cumulative_realty_rent_change_monthly_rate, cumulative_cash_conservative_investment_rate=cumulative_cash_conservative_investment_rate)
+avant_appartment = RealtyObject(name = 'Avant',  instant_price_rur=6000000, instant_price_renovation_rur=2500000, renovation_principal_and_interest_rur=0, renovation_principal_and_interest_payments_months=0, realty_mortgage_principal_and_interest_rur=254222, realty_mortgage_principal_and_interest_payments_months=30*12, cap_ex_rur=5000, income_tax_percentage=0.07, property_management_rur=20000, insurance_rur=0, additional_monthly_expenses_rur=0, utilities_rur=0, date_of_getting_keys=date(2027, 6, 1),  renovation_time_months=3, realty_object_price_rub=22000000, expected_monthly_rent_rur=120000, additional_income_rur=0,vacancy_percentage=0.9, cumulative_inflation_rate=cumulative_inflation_rate, cumulative_realty_price_change_monthly_rate=cumulative_realty_price_change_monthly_rate, cumulative_realty_rent_change_monthly_rate=cumulative_realty_rent_change_monthly_rate)
 
 # def calculate_income(asset: AssetBase, number_of_months = number_of_months):
 #   months_range = range(number_of_months) # 30 years
@@ -199,26 +194,25 @@ avant_appartment = RealtyObject(name = 'Avant',  instant_price_rur=6000000, inst
 #     income_from_asset_rub.append(last_income_from_asset_rub)
 #   return {'months_range': months_range, f'{asset.name} income': income_from_asset_rub}
 
-def prepare_data_frame(data: np.array, number_of_months = number_of_months):
+def prepare_data_frame(data: np.array, name: str):
   months_range = range(len(data)) # Ensure the range matches the length of data
-  return {'months_range': months_range, 'value': data}
+  return {'months_range': months_range, name: data}
 
 import matplotlib.pyplot as plt
 
 plt.matplotlib.use('Qt5Agg')
-number_of_months_to_plot : int = 1000
-avant_df_instant_costs = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_instant_costs(), number_of_months_to_plot))
-avant_df_monthly_costs = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_cumulative_monthly_costs(), number_of_months_to_plot))
-avant_df_instant_income = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_instant_income(), number_of_months_to_plot))
-avant_df_monthly_income = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_cumulative_monthly_income(), number_of_months_to_plot))
+avant_df_instant_costs = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_instant_costs(), "avant_df_instant_costs"))
+avant_df_monthly_costs = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_cumulative_monthly_costs(), "avant_df_monthly_costs"))
+avant_df_instant_income = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_instant_income(), "avant_df_instant_income"))
+avant_df_monthly_income = pd.DataFrame.from_dict(prepare_data_frame(avant_appartment.alltime_cumulative_monthly_income(), "avant_df_monthly_income"))
 
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(8, 6))
 axes = axes.flatten()  # Flatten the 2D array to 1D
 
 # Plot a bar graph
-avant_df_instant_costs.plot(ax=axes[0], x="months_range", y="value", kind="line")
-avant_df_monthly_costs.plot(ax=axes[1], x="months_range", y="value", kind="line")
-avant_df_instant_income.plot(ax=axes[2], x="months_range", y="value", kind="line")
-avant_df_monthly_income.plot(ax=axes[3], x="months_range", y="value", kind="line")
+avant_df_instant_costs.plot(ax=axes[0], x="months_range", y="avant_df_instant_costs", kind="line")
+avant_df_monthly_costs.plot(ax=axes[1], x="months_range", y="avant_df_monthly_costs", kind="line")
+avant_df_instant_income.plot(ax=axes[2], x="months_range", y="avant_df_instant_income", kind="line")
+avant_df_monthly_income.plot(ax=axes[3], x="months_range", y="avant_df_monthly_income", kind="line")
 
 plt.show()
